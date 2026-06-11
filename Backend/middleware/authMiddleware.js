1daca842fd487d1e8b59146e5fa3d8af30a26039
Mandar Shekatkar <167757210+MandarShekatkar@users.noreply.github.com>
2026-06-11 22:23:52 +0530

    Update authMiddleware.js

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const protect = async(req, res, next)=>{
         if(!token){
             return res.json({
                 success:false,
-                message:"Not Authorized, No Token",
+                message:"Not Authorized",
             });
         }
         // verify token
